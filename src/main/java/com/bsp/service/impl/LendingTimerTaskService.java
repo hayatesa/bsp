9933e7f4f8e5b2a79bb8a59ec5b798be5909ba82
wujunbiao1002 <820297837@qq.com>
2018-05-17 23:11:33 +0800

    借出人送书到运营点超时处理

--- src/main/java/com/bsp/service/impl/LendingTimerTaskService.java
+++ src/main/java/com/bsp/service/impl/LendingTimerTaskService.java
@@ -1,6 +1,8 @@
 package com.bsp.service.impl;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -61,27 +63,22 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 		// 数据库所有的借出记录
 		List<LendingRecord> lendingRecords = lendingRecordMapper.selectAllLendingRecord();
 
+
 		// 借出人超时未同意天数
-		int overtime_book_apply_check = Integer.valueOf((mappingMapper
-				.selectByPrimaryKey("overtime_book_apply_check").getmValue()));
-		// 借出人超时未同意天数
-		int overtime_bring_to_transfer_station = Integer.valueOf((mappingMapper
-				.selectByPrimaryKey("overtime_bring_to_transfer_station").getmValue()));
-		// 借出人超时未同意天数
-		int overtime_take_from_transfer_station = Integer.valueOf((mappingMapper
-				.selectByPrimaryKey("overtime_take_from_transfer_station").getmValue()));
+/*		int overtime_take_from_transfer_station = Integer.valueOf((mappingMapper
+				.selectByPrimaryKey("overtime_take_from_transfer_station").getmValue()));*/
 		List<MailEntity> mailBeans = new ArrayList<MailEntity>();	// 批量存放邮件
 		List<News> newss = new ArrayList<News>();	// 批量存放通知消息
 		for (LendingRecord lendingRecord : lendingRecords) {
 			if(lendingRecord.getLrStruts() == 0){ 
 				isOvertimeAgreeApply(lendingRecord, mailBeans, newss, nowDate);
-			}else if (lendingRecord.getLrStruts() == 2) {
-				
 			}else if (lendingRecord.getLrStruts() == 4) {
-				
+				isOvertimeBringToTransferStation(lendingRecord, mailBeans, newss, nowDate);
 			}else if (lendingRecord.getLrStruts() == 6) {
 				
 			}else if (lendingRecord.getLrStruts() == 8) {
+				
+			}else if (lendingRecord.getLrStruts() == 10) {
 				
 			}
 		}
@@ -91,7 +88,6 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 			MailSendUtils mailSendUtils = new MailSendUtils();
 			try {
 				mailSendUtils.sendMail(mailBean.getToMail(), mailBean.getSubject(),mailBean.getContent());// 发送邮件
-				System.out.println("mail ok");
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new  SendEmailException("发送借阅申请失效提醒失败，系统异常");
@@ -103,72 +99,76 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 			insertNews(news);
 		}
 	}
-	
 	/**
-	 * 转态为0表示借书人提交了借书申请，判断借出人是否超时未同意借书人的借书请求。
+	 * 状态为4表示借出人同意借出，判断借出人是否逾期未把图书送到运营点
 	 * @param lendingRecord
 	 * @param mailBeans
+	 * @param newss
+	 * @param nowDate
 	 */
-	private void isOvertimeAgreeApply(LendingRecord lendingRecord, List<MailEntity> mailBeans, List<News> newss, Date nowDate){
-		// 借出人超时未同意天数
-		int overtime_agree_apply = Integer.valueOf((mappingMapper
-				.selectByPrimaryKey("overtime_agree_apply").getmValue()));
-
-		// 提交借书的时间
-		Date createTime = lendingRecord.getCreateTime();
-		
+	private void isOvertimeBringToTransferStation(LendingRecord lendingRecord, List<MailEntity> mailBeans,
+			List<News> newss, Date nowDate) {
+		// 借出人超时未将图书送到运营点的天数
+		int overtimeBringToTransferStation = Integer.valueOf((mappingMapper
+				.selectByPrimaryKey("overtime_bring_to_transfer_station").getmValue()));
+		// 借出人同意借阅的时间
+		Date agreeTime = lendingRecord.getAgreeTime();
+				
 		// 相差的时间秒数
-		int seconds = (int) ((nowDate.getTime() - createTime.getTime()) / 1000);
-		
-		if (seconds >= (overtime_agree_apply - 2) * daySeconds 
-				&& seconds < ((overtime_agree_apply - 2) * daySeconds + intervalUpdateSeconds)) {
+		long seconds = (nowDate.getTime() - agreeTime.getTime()) / 1000;
+		// 最后的期限
+		String deadline = getDeadline(agreeTime, overtimeBringToTransferStation);
+
+		if (seconds >= (overtimeBringToTransferStation - 2) * daySeconds 
+				&& seconds < ((overtimeBringToTransferStation - 2) * daySeconds + intervalUpdateSeconds)) {
 			/*
-			 * 还有两天时间就超时，但借出人没有同意借阅者的图书借阅申请。
+			 * 还有两天时间就超时，但借出人没有将书籍送到运营点。
 			 * 发送通知信息到借出人账号和发送邮件提醒。
 			 */
 			String destMail = lendingRecord.getLoanableBook().getUser().getMail();
 			// 生成邮件主题内容以及添加邮件到mailBeans
-			String subject = "借阅申请提醒"; // 邮件主题
-			String content = "您的《"+lendingRecord.getLoanableBook().getLbName()
-					+"》图书正在被申请借阅，距离申请失效还有两天时间，请你尽快同意或拒接申请。谢谢!!!"; // 邮件主体内容
+			String subject = "送书提醒"; // 邮件主题
+			String content = "您同意借出的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书，距离最迟送达运营点时间的还有两天(最后期限为："+deadline+")，请你尽快将图书送到运营点。谢谢!!!"; // 邮件主体内容
 			MailEntity mailBean = new MailEntity(destMail,subject,content);
 			mailBeans.add(mailBean);
 			
 			// 发送账号通知信息
 			News news = new News(subject, new Date(), lendingRecord.getLoanableBook().getUser(), content);
 			newss.add(news);
-		}else if (seconds >= (overtime_agree_apply - 1) * daySeconds 
-				&& seconds < ((overtime_agree_apply - 1) * daySeconds + intervalUpdateSeconds)) {
+		}else if (seconds >= (overtimeBringToTransferStation - 1) * daySeconds 
+				&& seconds < ((overtimeBringToTransferStation - 1) * daySeconds + intervalUpdateSeconds)) {
 			/*
-			 * 还有一天时间就超时，但借出人没有同意借阅者的图书借阅申请。
+			 * 还有一天时间就超时，但借出人没有将书籍送到运营点。
 			 * 发送通知信息到借出人账号和发送邮件提醒。
 			 */
 			String destMail = lendingRecord.getLoanableBook().getUser().getMail();
 			// 生成邮件主题内容以及添加邮件到mailBeans
-			String subject = "借阅申请提醒"; // 邮件主题
-			String content = "您的《"+lendingRecord.getLoanableBook().getLbName()
-					+"》图书正在被申请借阅，距离申请失效还有一天时间，请你尽快同意或拒接申请。谢谢!!!"; // 邮件主体内容
+			String subject = "送书提醒"; // 邮件主题
+			String content = "您同意借出的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书，距离最迟送达运营点时间的还有一天(最后期限为："+deadline+")，请你尽快将图书送到运营点。谢谢!!!"; // 邮件主体内容
 			MailEntity mailBean = new MailEntity(destMail,subject,content);
 			mailBeans.add(mailBean);
 
 			// 发送账号通知信息
 			News news = new News(subject, new Date(), lendingRecord.getLoanableBook().getUser(), content);
 			newss.add(news);
-		}else if (seconds >= overtime_agree_apply * daySeconds) {
+		}else if (seconds >= overtimeBringToTransferStation * daySeconds) {
 			/*
-			 * 借阅申请已经超时，但借出人没有同意借阅者的图书借阅申请。
+			 * 送书期限已经超时，但借出人没有将书籍送到运营点。
 			 * 发送通知信息到借出人账号和发送邮件提醒。
 			 * 发送通知信息到借阅人账号和发送邮件提醒。
-			 * 把该条记录移到lending_history表中，并修改状态为3表示申请超时，删除lending_record表中的记录
+			 * 把该条记录移到lending_history表中，并修改状态为5表示借出人超时未送达，删除lending_record表中的记录
 			 */
 			String loanMail = lendingRecord.getLoanableBook().getUser().getMail();
 			String borrowMail = lendingRecord.getUser().getMail();
+			
 			// 生成邮件主题内容以及添加邮件到mailBeans
-			String subject = "借阅申请失效提醒"; // 邮件主题
-			String loanContent = "您的《"+lendingRecord.getLoanableBook().getLbName()
-					+"》图书被申请借阅，由于你超时未操作申请，系统自动已帮您取消了借阅申请。谢谢!!!"; // 给借出人邮件主体内容
+			String subject = "送书逾期提醒"; // 邮件主题
+			String loanContent = "您同意借出的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书，由于你超时未将图书送到运营点(最后期限为："+deadline+")，系统已自动帮您取消了借阅同意。谢谢!!!"; // 给借出人邮件主体内容
 			String borrowContent = "您申请借阅的《"+lendingRecord.getLoanableBook().getLbName()
-					+"》图书，由于对方超时未操作申请，系统已自动取消了借阅申请。你可以去寻找别的图书进行借阅。谢谢!!!"; // 给借阅人邮件主体内容	
+					+"》图书，由于对方超时未将图书送到运营点(最后期限为："+deadline+")，系统已自动帮您取消了借阅。你可以去寻找别的图书进行借阅。谢谢!!!"; // 给借阅人邮件主体内容	
 			MailEntity mailBean1 = new MailEntity(loanMail,subject,loanContent);
 			MailEntity mailBean2 = new MailEntity(borrowMail,subject,borrowContent);
 			mailBeans.add(mailBean1);
@@ -181,24 +181,160 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 			newss.add(news2);
 			
 			//把该条记录移到lending_history表中，并修改状态为3表示申请超时
-			Administrator administrator = new Administrator();
-			LendingHistory lendingHistory = new LendingHistory();
-			lendingHistory.setLhId(lendingRecord.getLrId());
-			lendingHistory.setCreateTime(lendingRecord.getCreateTime());
-			lendingHistory.setLhStruts((byte)3);
-			lendingHistory.setLoanPhone(lendingRecord.getLoanPhone());
-			lendingHistory.setLoanableBook(lendingRecord.getLoanableBook());
-			lendingHistory.setUser(lendingRecord.getUser());
-			lendingHistory.setReceiveAdmin(administrator);
-			lendingHistory.setBackAdmin(administrator);
+			LendingHistory lendingHistory = createLendingHistory(lendingRecord,(byte)5);
+			
 			insertLendingHistory(lendingHistory); // insert到历史表中
 			
 			// 删除lending_record表中的该记录
 			deleteLendingRecordByPrimaryKey(lendingRecord.getLrId());
+		}
+	}
+
+
+
+	/**
+	 * 状态为0表示借书人提交了借书申请，判断借出人是否超时未同意借书人的借书请求。
+	 * @param lendingRecord
+	 * @param mailBeans
+	 */
+	private void isOvertimeAgreeApply(LendingRecord lendingRecord, List<MailEntity> mailBeans,
+			List<News> newss, Date nowDate){
+		// 借出人超时未同意天数
+		int overtimeAgreeApply = Integer.valueOf((mappingMapper
+				.selectByPrimaryKey("overtime_agree_apply").getmValue()));
+
+		// 提交借书的时间
+		Date createTime = lendingRecord.getCreateTime();
+		
+		// 相差的时间秒数
+		long seconds = (nowDate.getTime() - createTime.getTime()) / 1000;
+		// 最后的期限
+		String deadline = getDeadline(createTime, overtimeAgreeApply);
+		if (seconds >= (overtimeAgreeApply - 2) * daySeconds 
+				&& seconds < ((overtimeAgreeApply - 2) * daySeconds + intervalUpdateSeconds)) {
+			/*
+			 * 还有两天时间就超时，但借出人没有同意借阅者的图书借阅申请。
+			 * 发送通知信息到借出人账号和发送邮件提醒。
+			 */
+			String destMail = lendingRecord.getLoanableBook().getUser().getMail();
+			// 生成邮件主题内容以及添加邮件到mailBeans
+			String subject = "借阅申请提醒"; // 邮件主题
+			String content = "您的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书正在被申请借阅，距离申请失效还有两天时间(最后期限为："+deadline+")，请你尽快同意或拒接申请。谢谢!!!"; // 邮件主体内容
+			MailEntity mailBean = new MailEntity(destMail,subject,content);
+			mailBeans.add(mailBean);
 			
+			// 发送账号通知信息
+			News news = new News(subject, new Date(), lendingRecord.getLoanableBook().getUser(), content);
+			newss.add(news);
+		}else if (seconds >= (overtimeAgreeApply - 1) * daySeconds 
+				&& seconds < ((overtimeAgreeApply - 1) * daySeconds + intervalUpdateSeconds)) {
+			/*
+			 * 还有一天时间就超时，但借出人没有同意借阅者的图书借阅申请。
+			 * 发送通知信息到借出人账号和发送邮件提醒。
+			 */
+			String destMail = lendingRecord.getLoanableBook().getUser().getMail();
+			// 生成邮件主题内容以及添加邮件到mailBeans
+			String subject = "借阅申请提醒"; // 邮件主题
+			String content = "您的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书正在被申请借阅，距离申请失效还有一天时间(最后期限为："+deadline+")，请你尽快同意或拒接申请。谢谢!!!"; // 邮件主体内容
+			MailEntity mailBean = new MailEntity(destMail,subject,content);
+			mailBeans.add(mailBean);
+
+			// 发送账号通知信息
+			News news = new News(subject, new Date(), lendingRecord.getLoanableBook().getUser(), content);
+			newss.add(news);
+		}else if (seconds >= overtimeAgreeApply * daySeconds) {
+			/*
+			 * 借阅申请已经超时，但借出人没有同意借阅者的图书借阅申请。
+			 * 发送通知信息到借出人账号和发送邮件提醒。
+			 * 发送通知信息到借阅人账号和发送邮件提醒。
+			 * 把该条记录移到lending_history表中，并修改状态为3表示申请超时，删除lending_record表中的记录
+			 */
+			String loanMail = lendingRecord.getLoanableBook().getUser().getMail();
+			String borrowMail = lendingRecord.getUser().getMail();
+			
+			// 生成邮件主题内容以及添加邮件到mailBeans
+			String subject = "借阅申请失效提醒"; // 邮件主题
+			String loanContent = "您的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书被申请借阅，由于你超时未操作申请(最后期限为："+deadline+")，系统已自动帮您取消了借阅申请。谢谢!!!"; // 给借出人邮件主体内容
+			String borrowContent = "您申请借阅的《"+lendingRecord.getLoanableBook().getLbName()
+					+"》图书，由于对方超时未操作申请(最后期限为："+deadline+")，系统已自动取消了借阅申请。你可以去寻找别的图书进行借阅。谢谢!!!"; // 给借阅人邮件主体内容	
+			MailEntity mailBean1 = new MailEntity(loanMail,subject,loanContent);
+			MailEntity mailBean2 = new MailEntity(borrowMail,subject,borrowContent);
+			mailBeans.add(mailBean1);
+			mailBeans.add(mailBean2);
+
+			// 发送账号通知信息
+			News news1 = new News(subject, new Date(), lendingRecord.getLoanableBook().getUser(), loanContent);
+			News news2 = new News(subject, new Date(), lendingRecord.getUser(), borrowContent);
+			newss.add(news1);
+			newss.add(news2);
+			
+			//把该条记录移到lending_history表中，并修改状态为3表示申请超时
+			LendingHistory lendingHistory = createLendingHistory(lendingRecord,(byte)3);
+			
+			insertLendingHistory(lendingHistory); // insert到历史表中
+			
+			// 删除lending_record表中的该记录
+			deleteLendingRecordByPrimaryKey(lendingRecord.getLrId());
 		}
 	}
 	
+	/**
+	 * 计算最后期限并转化为String
+	 * @param Date
+	 * @param overtime
+	 * @return String
+	 */
+	private String getDeadline(Date agreeTime, int overtime) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(agreeTime);
+		calendar.add(Calendar.DAY_OF_MONTH, overtime);
+		Date deadline = calendar.getTime();
+		SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		return sdf.format(deadline);
+	}
+	
+	/**
+	 * 根据lendingRecord对象信息创建lendingHistory对象
+	 * @param lendingRecord
+	 * @param status
+	 * @return lendingRecord对象
+	 */
+	private LendingHistory createLendingHistory(LendingRecord lendingRecord, byte status) {
+		Administrator administrator = new Administrator();
+		LendingHistory lendingHistory = new LendingHistory();
+		
+		lendingHistory.setLhId(lendingRecord.getLrId());
+		lendingHistory.setCreateTime(lendingRecord.getCreateTime());
+		lendingHistory.setSendToTime(lendingRecord.getSendToTime());
+		lendingHistory.setTakeAwayTime(lendingRecord.getTakeAwayTime());
+		lendingHistory.setExpectedReturnTime(lendingRecord.getExpectedReturnTime());
+		lendingHistory.setActualReturnTime(lendingRecord.getActualReturnTime());
+		lendingHistory.setTakeBackTime(lendingRecord.getTakeBackTime());
+		lendingHistory.setLhStruts(status);
+		lendingHistory.setLoanPhone(lendingRecord.getLoanPhone());
+		lendingHistory.setLoanableBook(lendingRecord.getLoanableBook());
+		lendingHistory.setUser(lendingRecord.getUser());
+		if(lendingRecord.getReceiveAdmin() != null){
+			lendingHistory.setReceiveAdmin(lendingRecord.getReceiveAdmin());
+		}else {
+			lendingHistory.setReceiveAdmin(administrator);
+		}
+		if(lendingRecord.getReceiveAdmin() != null){
+			lendingHistory.setBackAdmin(lendingRecord.getBackAdmin());
+		}else {
+			lendingHistory.setBackAdmin(administrator);
+		}
+		return lendingHistory;
+	}
+
+	/**
+	 * 根据主键删除lending_record表中的记录
+	 * @param lrId
+	 */
 	@Transactional
 	private void deleteLendingRecordByPrimaryKey(Integer lrId) {
 		try {
@@ -209,6 +345,10 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 		}
 	}
 
+	/**
+	 * 向lending_history表中插入记录
+	 * @param lendingHistory
+	 */
 	@Transactional
 	private void insertLendingHistory(LendingHistory lendingHistory) {
 		try {
@@ -219,6 +359,10 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 		}
 	}
 
+	/**
+	 * 向news表中插入通知消息
+	 * @param news
+	 */
 	@Transactional
 	private void insertNews(News news){
 		try {
@@ -228,4 +372,6 @@ public class LendingTimerTaskService implements ILendingTimerTaskService{
 			throw new SystemErrorException("操作失败，系统异常");
 		}
 	}
+	
+	
 }
